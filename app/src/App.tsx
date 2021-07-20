edfc8a51b955377b246487edb0bc260714a4544d
Yair Dana <yair.dana1991@gmail.com>
2021-07-20 13:57:07 +0300

    initial public data

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
   const [landingPageData, setLandingPageData] = useState(JsonData)
   useEffect(() => {
     setLandingPageData(JsonData)
-  }, [])
+  }, [])x
 
   return (
     <div>
