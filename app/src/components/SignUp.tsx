3f0b9a016aedda07f22b789974c66a02900aa164
Yair Dana <yair.dana1991@gmail.com>
2021-07-27 14:11:38 +0300

    configs and api client and lint

--- app/src/components/SignUp.tsx
+++ app/src/components/SignUp.tsx
@@ -1,51 +1,49 @@
 
-export const SignUp = (props:any) => {
-    return (
-      <div id='team' className='text-center'>
-        <div className='container'>
-          <div className='col-md-8 col-md-offset-2 section-title'>
-            <h2>Sign Up</h2>
-            <p>
-              For using our platform, you should first sign up
-            </p>
-<div>
-          <form>
+export const SignUp = (props: any) => {
 
-          <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+  
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>Sign Up</h2>
+          <p>
+            For using our platform, you should first sign up
+          </p>
+          <div>
+            <form>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+              <div className="form-group">
+                <label>First name</label>
+                <input type="text" className="form-control" placeholder="First name" />
+              </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+              <div className="form-group">
+                <label>Last name</label>
+                <input type="text" className="form-control" placeholder="Last name" />
+              </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <br></br>
-                <p>
-                    Already registered <a href="/signin">sign in?</a>
-                </p>
+              <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+              </div>
+
+              <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+              </div>
+
+              <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+              <br></br>
+              <p>
+                Already registered <a href="/signin">sign in?</a>
+              </p>
             </form>
-            </div>
-            </div>
-
+          </div>
         </div>
+
       </div>
-    )
-  }
-// export const SignIn = () => {
-//         return (
-           
-//         );
-// }
+    </div>
+  )
+}
