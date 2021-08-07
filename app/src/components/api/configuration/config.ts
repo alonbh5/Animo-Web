b9d4bef0d15c0a6b56f6e1e901e268ac79bded1b
Yair Dana <yair.dana1991@gmail.com>
2021-08-07 09:47:12 +0300

    sent email

--- app/src/components/api/configuration/config.ts
+++ app/src/components/api/configuration/config.ts
@@ -6,6 +6,7 @@ export const staticsPort = 3001;
 export const staticsUrl = `${host}:${staticsPort}/`;
 
 //emailjs
-export const templateId = 'template_tgvrnlt';
-export const serviceId = 'service_1jqh2bv';
-export const userID = 'user_6GUtgE72H2juyigM61uXw';
+export const templateId = 'template_e2no9zc';
+export const serviceId = 'service_03oxvhn';
+export const userID = 'user_AdbVf7bL75xL6zL117UXr';
+export const animoMail = '2021Animo@gmail.com'
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,13 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+//@ts-ignore
+import AlertTemplate from 'react-alert-template-basic'
 import {BrowserRouter} from 'react-router-dom'
-ReactDOM.render(<BrowserRouter>
-  <React.StrictMode>
+
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
+
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
+      <React.StrictMode>
+
+  <BrowserRouter>
     <App />
-  </React.StrictMode></BrowserRouter>,
-  document.getElementById('root')
-);
+    </BrowserRouter>
+    </React.StrictMode>
+  </AlertProvider>
+)
+ReactDOM.render(<Root />, document.getElementById('root'))
+
+
+
+// ReactDOM.render(
+//     <App />
+//   ,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
