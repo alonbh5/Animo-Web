c1a81bb106f0d223a2923d6bc890c184105ffc62
Shir Elfanbaum <shirelfanbaum@gamil.com>
2021-08-27 15:39:08 +0300

    merge remote

--- app/src/components/Pages/TipsAndArticals.tsx
+++ app/src/components/Pages/TipsAndArticals.tsx
@@ -3,11 +3,60 @@ export const TipsAndArticals = (props: any) => {
     <div id='team' className='text-center'>
       <div className='container'>
         <div className='col-md-8 col-md-offset-2 section-title'>
-          <h2>Tips And Articals</h2>
-          <div>
+        <h2>Tips And Articals</h2>
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+         </div>
+
+         <h1>Articals</h1>
+          <br></br>
+          <br></br>
+          <div className ="flex-container" >
+            <div className="grid-item"> 
+            <img src="https://www.evolutionpsychologycenter.ca/wp-content/uploads/2018/05/bigstock-210204367.jpg" alt='...' className='team-img' />
+            <br></br>
+            <br></br>
+            <a id="link" href="https://www.talkspace.com/blog/fear-of-change-why-life-adjustments-are-difficult" target="_blank"> fear of change why life adjustments are difficult </a>
+            </div>
+
+            <div className="grid-item" > 
+            <img src="https://www.optimistdaily.com/wp-content/uploads/Good-Life.jpg" alt='...' className='team-img' />
+            <br></br>
+            <br></br>
+            <a id="link" href="https://www.bakadesuyo.com/2021/07/awesome/" target="_blank">"The Lazy Way To An Awesome Life: 4 Secrets Backed By Research" </a>
+            </div>
+
+            <div className="grid-item"> 
+            <img src="https://f6h8q2y9.stackpathcdn.com/wp-content/uploads/2015/03/Happy-group-of-friends-family.jpg" alt='...' className='team-img' />
+            <br></br>
+            <br></br>
+            <a id="link" href="https://www.bakadesuyo.com/2021/03/happier-without-really-trying/" target="_blank">"How to Be Happier Without Really Trying" </a>
+            </div>
+            <div > 
+            <img src="https://safestart.com/wp-content/uploads/2019/12/HabitNotes-780x480.jpg" alt='...' className='team-img' />
+            <br></br>
+            <br></br>
+            <a id="link" href="https://www.bakadesuyo.com/2021/06/how-to-quit-bad-habits-2/" target="_blank">"This Is How To Quit Bad Habits Without Willpower: 4 Secrets From Neuro..." </a>
+            </div>
+          <div className="grid-item" > 
+          <img src=" https://www.wetheyoung.in/wp-content/uploads/2020/02/fomo.jpg" alt='...' className='team-img' />
+          <br></br>
+          <br></br>
+          <a id="link" href="https://time.com/4358140/overcome-fomo/" target="_blank">"fear of missing out" </a>
+          </div>
+          
+          <div className="grid-item" > 
+          <img src=" https://www.bakadesuyo.com/wp-content/uploads/2021/06/life-awesome.jpeg" alt='...' className='team-img' />
+          <br></br>
+          <br></br>
+          <a id="link" href="https://www.bakadesuyo.com/2021/06/life-awesome/" target="_blank">"This Is The Most Fun Way To Make Your Life Awesome" </a>
+          </div>
+          
+          </div>
+         <br></br>
+         <br></br>
+         <h1>Tips</h1>
+          </div>
+       
+    
+  )
+}
