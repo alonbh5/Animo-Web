12f4a7088dd7e769ca377224a80b53a545cd235b
Yair Dana <yair.dana1991@gmail.com>
2021-09-02 17:37:46 +0300

    port and nodemon

--- server/my_server.js
+++ server/my_server.js
@@ -1,8 +1,8 @@
 const http  = require('http');
 const app = require('./app');
-
+const port = process.env.PORT || 3000
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
 
-console.log("Server is Running on localhost:"+port);
+console.log("Server is Running on localhost:"+ port);
