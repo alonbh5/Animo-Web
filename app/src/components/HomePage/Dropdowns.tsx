8ffdab5838b9b35b192d044b9b0743cd6cc2de05
Yair Dana <yair.dana1991@gmail.com>
2021-09-11 12:45:39 +0300

    refactor dropdowns

---
+++ app/src/components/HomePage/Dropdowns.tsx
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+
+type DropdownProps = {
+    onClickItem: (value:boolean) => void;
+    Logout: () => void;
+  }
+
+export const AdminDropdown = (props:DropdownProps) => {
+  const _onClickItem = () => {
+    props.onClickItem(false);
+  };
+
+  return (<div className="dropdown-menu">
+    <li><Link to="/profile" onClick={_onClickItem}>Profile</Link></li>
+    <li><HashLink to="/messaging">Messanger</HashLink></li>
+    <li><Link to="/manageUsers" onClick={_onClickItem}>Manage Users</Link></li>
+    <li><Link to="/invitePsychologist" onClick={_onClickItem}>Invite Psycoligist</Link></li>
+    <li><Link to="/uploadTipsArticals" onClick={_onClickItem}>{'Upload Tips & Articals'}</Link></li>
+    <li><Link to="/confirmTips"onClick={_onClickItem}>{'Confirm Tips & Articals'}</Link></li>
+    <li><a onClick={() => props.Logout()}>Logout</a></li>
+  </div>
+  );
+};
+
+export const PsychologistDropdown = (props:DropdownProps) => {
+  const _onClickItem = () => {
+    props.onClickItem(false);
+  };
+
+  return (<div className="dropdown-menu">
+    <li><Link to="/profile" onClick={_onClickItem}>Profile</Link></li>
+    <li><HashLink to="/messaging">Messanger</HashLink></li>
+    <li><Link to="/aboutMePsycoligist" onClick={_onClickItem}>About Me</Link></li>
+    <li><Link to="/uploadTipsArticals" onClick={_onClickItem}>{'Upload Tips & Articals'}</Link></li>
+    <li><a onClick={() => props.Logout()}>Logout</a></li>
+  </div>
+  );
+};
+
+export const GeneralDropdown = (props:DropdownProps) => {
+  const _onClickItem = () => {
+    props.onClickItem(false);
+  };
+
+  return (
+    <div className="dropdown-menu">
+      <li><Link to="/profile" onClick={_onClickItem}>Profile</Link></li>
+      <li><HashLink to="/messaging">Messanger</HashLink></li>
+      <li><a onClick={() => props.Logout()}>Logout</a></li>
+    </div>
+
+  );
+};
---
+++ app/src/components/utils.ts
@@ -0,0 +1,2 @@
+/* eslint-disable max-len */
+export const avatarImage = 'https://icons.iconarchive.com/icons/icons8/ios7/256/Users-User-Male-2-icon.png';
